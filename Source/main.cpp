07675c550f2e758834519944cb7b293d035653d9
Konjointed <167786716+dfutia@users.noreply.github.com>
2024-08-14 14:53:02 -0400

    [+] editor class and imgui

--- Source/main.cpp
+++ Source/main.cpp
@@ -1,32 +1,45 @@
 #define _SILENCE_STDEXT_ARR_ITERS_DEPRECATION_WARNING
 
 #include "graphicscontext.h"
+#include "editor.h"
 #include "luaenvironment.h"
-#include "event.h"
 
 #include <spdlog/spdlog.h>
+#include <imgui_impl_sdl2.h>
 
 #include <iostream>
 
 int main(int argc, char* argv[]) {
 	GraphicsContext graphics;
-	LuaEnvironment luaEnvironment;
-
 	if (!graphics.initialize("Roblox Studio", 800, 600)) {
 		std::cerr << "Failed to iniitalize graphics context" << std::endl;
 		return -1;
 	}
 
+	Editor editor(graphics);
+	LuaEnvironment luaEnvironment;
+
 	bool running = true;
 	while (running) {
 		SDL_Event event;
 		while (SDL_PollEvent(&event)) {
-			if (event.type == SDL_QUIT) {
+			ImGui_ImplSDL2_ProcessEvent(&event);
+
+			switch (event.type) {
+			case SDL_QUIT:
 				running = false;
+				break;
+			case SDL_WINDOWEVENT:
+				if (event.window.event == SDL_WINDOWEVENT_CLOSE) {
+					running = false;
+				}
+				break;
 			}
 		}
 
 		glClear(GL_COLOR_BUFFER_BIT);
+
+		editor.render();
 
 		graphics.swapBuffers();
 	}
